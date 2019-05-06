5e0800e0f72901940090a82c1a4d1819895fbc3d
Gabriel Montalvo <Gabriel09199@gmail.com>
2019-05-05 21:28:59 -0500

    Vista de recordatorio y notificacion terminada, progreso vista de detalla asignatura y asignaturas

---
+++ uhelper/app/src/main/java/com/techdevcol/uhelper/activities/DetalleAsignatura.java
@@ -0,0 +1,14 @@
+package com.techdevcol.uhelper.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.techdevcol.uhelper.R;
+
+public class DetalleAsignatura extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle_asignatura);
+    }
+}
