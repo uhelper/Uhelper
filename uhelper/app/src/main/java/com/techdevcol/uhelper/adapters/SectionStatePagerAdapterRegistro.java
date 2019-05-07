c4987749651d10cc16015561adb4e65ceed45eee
sercel98 <36521785+sercel98@users.noreply.github.com>
2019-05-07 01:28:33 -0500

    Realizando el formulario de registro

--- uhelper/app/src/main/java/com/techdevcol/uhelper/adapters/SectionStatePagerAdapterRegistro.java
+++ uhelper/app/src/main/java/com/techdevcol/uhelper/adapters/SectionStatePagerAdapterRegistro.java
@@ -1,4 +1,33 @@
 package com.techdevcol.uhelper.adapters;
 
-public class SectionStatePagerAdapterRegistro {
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SectionStatePagerAdapterRegistro extends FragmentStatePagerAdapter {
+
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+
+
+    public SectionStatePagerAdapterRegistro(FragmentManager fm) {
+        super(fm);
+    }
+
+
+    public void addFragment(Fragment fragment) {
+        mFragmentList.add(fragment);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return mFragmentList.get(i);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
 }
--- uhelper/app/src/main/java/com/techdevcol/uhelper/fragments/FragmentRegistro1.java
+++ uhelper/app/src/main/java/com/techdevcol/uhelper/fragments/FragmentRegistro1.java
@@ -1,4 +1,33 @@
 package com.techdevcol.uhelper.fragments;
 
-public class FragmentRegistro1 {
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.techdevcol.uhelper.R;
+
+public class FragmentRegistro1 extends Fragment {
+
+    private TextView txtNombres;
+    private TextView txtApellidos;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_registro1, container, false);
+
+        txtNombres = (TextView) view.findViewById(R.id.txtNombres);
+        txtApellidos = (TextView) view.findViewById(R.id.txtApellidos);
+
+        return view;
+    }
 }
+
+
+
